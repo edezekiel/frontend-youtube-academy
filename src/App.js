15eabaa281b66621db86885f3bd4507a63d5208a
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-28 16:49:59 -0500

    begin implementing google auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
 import React, { Component } from 'react';
-
+import { GoogleLogin } from 'react-google-login';
+import ReactDOM from 'react-dom'
 const URL = "http://localhost:3000/api/v1/users"
 
+
 class App extends Component {
+
+  state = {
+    isAuthenticated: false,
+    user: null,
+    token: ''
+  }
+
+  logout = () => {
+    this.setState({isAuthenticated: false, token: '', user: null})
+  }
+
+  onFailure = (error) => { alert(error) }
+
+  responseGoogle = (response) => {
+      const tokenBlob = new Blob([JSON.stringify({access_token: response.accessToken}, null, 2)], {type : 'application/json'});
+      const options = {
+          method: 'POST',
+          body: tokenBlob,
+          mode: 'cors',
+          cache: 'default'
+      };
+      fetch('http://localhost:3000/api/v1/auth/google', options).then(r => {
+          const token = r.headers.get('x-auth-token');
+          r.json().then(user => {
+              if (token) {
+                  this.setState({isAuthenticated: true, user, token})
+              }
+          });
+      })
+  };
 
   componentDidMount() {
     fetch(URL)
@@ -13,11 +45,12 @@ class App extends Component {
   render() {
 
     return (
-      <div>
-        <p>Log in with Google</p>
-        <button id="authorize-button">Log In</button>
-        <button id="signout-button">Log Out</button>
-      </div>
+      <GoogleLogin
+        clientId="369548765069-c8i73drmbk5q2s47bumpkltp6ebjsljl.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.onFailure}
+        />
     );
   }
 }
