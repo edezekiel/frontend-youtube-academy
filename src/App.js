78b539fbd731c1b8b981beeaaf67be0513b99140
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-28 15:29:26 -0500

    add URL to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
+const URL = "https://localhost/3000"
+
 class App extends Component {
+
+  connectBackend = () => {
+      fetch(URL)
+      .then(res => res.json())
+      .then(console.log)
+  }
+
   render() {
     return (
-      <div>Hello World</div>
+      <div>Hello World {this.connectBackend()}</div>
+
     );
   }
 }
