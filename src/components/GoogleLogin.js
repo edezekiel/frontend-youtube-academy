63707ee11493a34b7a2ff5947eeec9de279f1abf
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-29 15:51:59 -0500

    add more info to console log in GoogleLogin component

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -7,9 +7,6 @@ import API_KEY from '../services/Youtube'
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
-// const CLIENT_ID = '369548765069-mof01apa10i4vj53dk7g327j8oh91te7.apps.googleusercontent.com'
-// const API_KEY = 'AIzaSyDadIIN_F6iRgWfuLEFxYQ3fn1Pj8Ai2EU'
-
 class GoogleLogin extends Component {
 
   componentDidMount(){
@@ -31,11 +28,11 @@ class GoogleLogin extends Component {
 
         // if logged in, dispatch user: true to state
         // also need another gapi.auth2 call?
-        console.log("user is logged in")
+        console.log("user is logged in", window.gapi.auth2.getAuthInstance())
         :
 
         // else ... ?
-        console.log('user is not logged in')
+        console.log('user is not logged in', window.gapi.auth2.getAuthInstance())
     });
   }
 
