b7c91a8efeeb576ae9006f552b5c51c6cc22a552
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-29 10:47:11 -0500

    begin implmenting redux

---
+++ src/reducers.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+  loggedIn: false,
+  user: null
+}
+
+function rootReducer = (state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state
+  }
+}
+
+export default rootReducer
