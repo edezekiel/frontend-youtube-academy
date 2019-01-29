f3acab1d5745216dfd87bd6a2a1d4fe8cc27de9e
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-29 08:34:49 -0500

    added ternary to response function

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const URL = "http://localhost:3000/api/v1/users"
 class App extends Component {
 
   responseGoogle = (response) => {
-    console.log(response)
+    response.error ? console.log("there was an error:", response.error) : console.log("successfully logged in", response)
   }
 
   logout = () => {
@@ -23,17 +23,17 @@ class App extends Component {
 
     return (
       <div>
-      <GoogleLogin
-      clientId="369548765069-mof01apa10i4vj53dk7g327j8oh91te7.apps.googleusercontent.com"
-      buttonText="Login"
-      onSuccess={this.responseGoogle}
-      onFailure={this.responseGoogle}
-      />
-      <GoogleLogout
-        buttonText="Logout"
-        onLogoutSuccess={this.logout}
-      >
-      </GoogleLogout>
+        <GoogleLogin
+        clientId="369548765069-mof01apa10i4vj53dk7g327j8oh91te7.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={this.responseGoogle}
+        onFailure={this.responseGoogle}
+        />
+        <GoogleLogout
+          buttonText="Logout"
+          onLogoutSuccess={this.logout}
+        >
+        </GoogleLogout>
       </div>
     );
   }
