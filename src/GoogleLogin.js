750473ac1132610f040fddc32524c570f549d0ac
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-29 09:19:35 -0500

    getAuthInstance back using window.gapi

--- src/GoogleLogin.js
+++ src/GoogleLogin.js
@@ -4,6 +4,8 @@ const CLIENT_ID = '369548765069-mof01apa10i4vj53dk7g327j8oh91te7.apps.googleuser
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"];
 const SCOPES = 'https://www.googleapis.com/auth/youtube.readonly';
 
+const API_KEY = 'AIzaSyDadIIN_F6iRgWfuLEFxYQ3fn1Pj8Ai2EU'
+
 export default class GoogleLogin extends Component {
 
   componentDidMount(){
@@ -11,8 +13,22 @@ export default class GoogleLogin extends Component {
   }
 
   handleClientLoad = () => {
-    console.log('handleclientload')
-    // gapi.load('client:auth2', initClient)
+    window.gapi.load('client:auth2', this.initClient)
+  }
+
+  initClient = () => {
+    console.log('init client')
+    window.gapi.client.init({
+      discoveryDocs: DISCOVERY_DOCS,
+      clientId: CLIENT_ID,
+      scope: SCOPES
+    }).then(function () {
+      console.log(window.gapi.auth2.getAuthInstance())
+      // .isSignedIn.listen(updateSigninStatus);
+      // updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+      // authorizeButton.onclick = handleAuthClick;
+      // signoutButton.onclick = handleSignoutClick;
+    });
   }
 
   render() {
