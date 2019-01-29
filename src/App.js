d7e90f54288ec9a8fe0c3e886af0e0b3c12a4379
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-29 08:21:43 -0500

    add google login button to frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 
 const URL = "http://localhost:3000/api/v1/users"
 
 class App extends Component {
+
+  responseGoogle = (response) => {
+    console.log(response)
+  }
+
+  logout = () => {
+    console.log('logout')
+  }
 
   componentDidMount() {
     fetch(URL)
@@ -13,7 +22,19 @@ class App extends Component {
   render() {
 
     return (
-      <div>Hi there</div>
+      <div>
+      <GoogleLogin
+      clientId="369548765069-mof01apa10i4vj53dk7g327j8oh91te7.apps.googleusercontent.com"
+      buttonText="Login"
+      onSuccess={this.responseGoogle}
+      onFailure={this.responseGoogle}
+      />
+      <GoogleLogout
+        buttonText="Logout"
+        onLogoutSuccess={this.logout}
+      >
+      </GoogleLogout>
+      </div>
     );
   }
 }
