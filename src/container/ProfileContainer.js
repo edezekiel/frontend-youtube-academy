bab1847d23613ce51dcac91b467caa36410b8d09
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-01-31 12:26:33 -0500

    debug initialState issues

--- src/container/ProfileContainer.js
+++ src/container/ProfileContainer.js
@@ -5,9 +5,13 @@ import Profile from '../presentational/Profile'
 import RAILS_API from '../services/Backend'
 
 class ProfileContainer extends Component {
+  componentDidMount(){
+    // if (!this.props.user) => Re
+  }
+
   render() {
     return (
-      <div>{this.props.user ? <Profile /> : null}</div>
+      <Profile />
     )
   }
 }
