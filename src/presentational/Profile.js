5048456f0662823463b11e98a586d794db437ec9
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-01 14:03:37 -0500

    add user info to profile page

--- src/presentational/Profile.js
+++ src/presentational/Profile.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-import { Segment, Container, Header } from 'semantic-ui-react'
+import { Segment, Container, Header, Card, Image } from 'semantic-ui-react'
 
 const Profile = (props) => {
+
   return(
     <Segment>
-      <Container textAlign="center">
-        <Header>Profile Page</Header>
-      </Container>
+    <Header>Welcome {JSON.parse(localStorage.user).w3.ig}!</Header>
+    <Image src={JSON.parse(localStorage.user).w3.Paa} />
+
     </Segment>
   )
 }
