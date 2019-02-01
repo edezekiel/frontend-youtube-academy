4eb1d45c1c74aa5a3af590eaae59e0a1be112ea7
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-01 10:39:56 -0500

    incorporate boilerplate code from youtube api docs into search container

--- src/container/SearchContainer.js
+++ src/container/SearchContainer.js
@@ -34,11 +34,84 @@ class SearchContainer extends Component {
     })
   }
 
+  //---------------------BOILERPLATE---------------------//
+
+  buildApiRequest = (requestMethod, path, params, properties) => {
+    params = this.removeEmptyParams(params);
+    let request;
+    if (properties) {
+      let resource = this.createResource(properties);
+      request = window.gapi.client.request({
+          'body': resource,
+          'method': requestMethod,
+          'path': path,
+          'params': params
+      });
+    } else {
+      request = window.gapi.client.request({
+          'method': requestMethod,
+          'path': path,
+          'params': params
+      });
+    }
+    this.executeRequest(request);
+  }
+
+  createResource = (properties) => {
+    let resource = {};
+    let normalizedProps = properties;
+    for (let p in properties) {
+      let value = properties[p];
+      if (p && p.substr(-2, 2) == '[]') {
+        let adjustedName = p.replace('[]', '');
+        if (value) {
+          normalizedProps[adjustedName] = value.split(',');
+        }
+        delete normalizedProps[p];
+      }
+    }
+    for (let p in normalizedProps) {
+      // Leave properties that don't have values out of inserted resource.
+      if (normalizedProps.hasOwnProperty(p) && normalizedProps[p]) {
+        let propArray = p.split('.');
+        let ref = resource;
+        for (let pa = 0; pa < propArray.length; pa++) {
+          let key = propArray[pa];
+          if (pa == propArray.length - 1) {
+            ref[key] = normalizedProps[p];
+          } else {
+            ref = ref[key] = ref[key] || {};
+          }
+        }
+      };
+    }
+    return resource;
+  }
+
+  removeEmptyParams = (params) => {
+    for (let p in params) {
+      if (!params[p] || params[p] == 'undefined') {
+        delete params[p];
+      }
+    }
+    return params;
+  }
+
+  executeRequest = (request) => {
+    request.execute(response => console.log(response));
+  }
+
   //---------------------SEARCH_YOUTUBE---------------------//
 
   search = () => {
-    let request = window.gapi.client.youtube.channels.list({'part': 'snippet', 'mine': 'true'});
-    request.execute(res => console.log(res))
+    // let request = window.gapi.client.youtube.channels.list({'part': 'snippet', 'mine': 'true'});
+    // request.execute(res => console.log(res))
+    this.buildApiRequest('GET',
+                '/youtube/v3/search',
+                {'maxResults': '25',
+                 'part': 'snippet',
+                 'q': 'surfing',
+                 'type': ''});
   }
 
   render() {
