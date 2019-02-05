5b839d39b6e000c77aa837aa4e2f95870887d919
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-05 18:14:18 -0500

    add background to login/logout, center the auth button

--- src/presentational/GoogleAuth.js
+++ src/presentational/GoogleAuth.js
@@ -1,24 +1,22 @@
-import React from 'react'
+import React from "react";
 
-import { Segment, Container, Button, Image, Card } from 'semantic-ui-react'
-import googleLogo from '../assets/google.png'
+import { Grid, Segment, Button, Icon } from "semantic-ui-react";
 
-const GoogleAuth = (props) => {
-
-  return(
-    <Segment basic>
-      <Container textAlign="center">
-        <Card centered>
-          <Image src={googleLogo} />
-          <Card.Content>
-            <Button onClick={props.authHandler} as='a' inverted color='red'>
+const GoogleAuth = props => {
+  return (
+    <Grid>
+      <Grid.Row style={{"height": '90vh'}}>
+        <Grid.Column>
+          <Segment style={{"display": "flex", "flex-direction": "column", "justify-content": "center", "align-items": "center", "height": '90vh', "background": "url(https://images.unsplash.com/photo-1519419166318-4f5c601b8e6c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80) no-repeat center center fixed"}}>
+            <Button size="massive" style={{"text-align": "center"}} onClick={props.authHandler} as="a" color="red">
+              <Icon name='google' />
               {props.message}
             </Button>
-          </Card.Content>
-        </Card>
-      </Container>
-    </Segment>
-  )
-}
+          </Segment>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
+};
 
-export default GoogleAuth
+export default GoogleAuth;
