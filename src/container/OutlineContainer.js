2d2aef80496e1b8bdf91c8e409f6946b54be0a90
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-07 15:10:31 -0500

    refactored OutlineContainer

--- src/container/OutlineContainer.js
+++ src/container/OutlineContainer.js
@@ -7,28 +7,16 @@ import { Link } from "react-router-dom";
 import Video from "../presentational/Video";
 
 class OutlineContainer extends Component {
-  // state = {
-  //   outline: null
-  // }
-  //
-  // componentDidMount(){
-  //   this.setState({outline: this.findOutline(this.props.outlines)[0]})
-  // }
-  //
-  // findOutline = (outlines) => {
-  //   return outlines.filter(outline => {
-  //     return outline.id === parseInt(this.props.match.params.id);
-  //   });
-  // };
 
   render() {
+    console.log(this.props)
     return (
       <Container>
-        {this.state.outline ?
+        {this.props.outline ?
           <Container>
           <Segment>
-            <Header>Outline: {this.state.outline.videoTitle}</Header>
-            <Link to={"/outlines/outlinenote/create/" + this.state.outline.id}>
+            <Header>Outline: {this.props.outline.videoTitle}</Header>
+            <Link to={"/outlines/outlinenote/create/" + this.props.outline.id}>
               <Button inverted color="red">
                 Add Outline To Notebook
               </Button>
@@ -36,11 +24,11 @@ class OutlineContainer extends Component {
           </Segment>
           <Segment>
             <Segment basic>
-              <Video videoId={this.state.outline.videoId} />
+              <Video videoId={this.props.outline.videoId} />
             </Segment>
             <Segment basic>
               <Header>Your Notes:</Header>
-              {this.state.outline.notes}
+              {this.props.outline.notes}
             </Segment>
           </Segment>
           </Container>
@@ -51,7 +39,9 @@ class OutlineContainer extends Component {
   }
 }
 
-let mapStateToProps = ({ user }) => {
-  return { user };
+let mapStateToProps = ({ user }, ownProps) => {
+  let outline = user.outlines.find(x => x.id === parseInt(ownProps.match.params.id))
+  return { user, outline };
 };
+
 export default connect(mapStateToProps)(OutlineContainer);
