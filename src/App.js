e42599adc8ea9094169e5c62335d67ce352031c9
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-08 15:15:09 -0500

    hard refresh refactoring

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 import NavBar from "./NavBar";
 
-import RootContainer from "./container/RootContainer";
 import GoogleAuthContainer from "./container/GoogleAuthContainer";
 import SearchContainer from "./container/SearchContainer";
 import Profile from "./container/Profile";
@@ -15,22 +15,65 @@ import UserNotebookContainer from "./container/UserNotebookContainer";
 import CreateNotebookForm from "./forms/CreateNotebookForm";
 import CreateOutlineNoteForm from "./forms/CreateOutlineNoteForm";
 
-const App = props => {
-  return (
-    <Container>
-      <NavBar />
-      <Switch>
-        <Route exact path="/logout" component={GoogleAuthContainer} />
-        <Route exact path="/search" component={SearchContainer} />
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/notebooks/create" component={CreateNotebookForm} />
-        <Route exact path="/outlines/outlinenote/create/:id" component={CreateOutlineNoteForm} />
-        <Route exact path="/outlines/:id" component={UserOutlineContainer} />
-        <Route exact path="/notebooks/:id" component={UserNotebookContainer} />
-        <Route exact path="/" component={RootContainer} />
-      </Switch>
-    </Container>
-  );
+
+//------------------BRAINSTORMING------------------//
+
+// <Route exact path="/login" component={GoogleAuthContainer} />
+// <Route exact path="/logout" component={GoogleAuthContainer} />
+
+// <Route exact path="/search" component={SearchContainer} />
+
+// <Route exact path="/search/{search_term}" component={SearchContainer} />
+
+
+  // <Route exact path="/outlines" component={__Outlines__} />
+    // <Route exact path="/outlines/outlinenote/create/:id" component={CreateOutline} />
+
+
+// <Route exact path="/profile" component={Profile} />
+
+  // <Route exact path="/outlines" component={__Outlines__} />
+    // <Route exact path="/outlines/:id" component={Outline} />
+    // <Route exact path="/outlines/outlinenote/create/:id" component={CreateOutline} />
+
+  // <Route exact path="/notebooks" component={__Notebooks__} />
+    // <Route exact path="/notebooks/:id" component={Notebook} />
+    // <Route exact path="/notebooks/create" component={CreateNotebook} />
+
+
+
+//------------------ORIGINAL------------------//
+// <Route exact path="/logout" component={GoogleAuthContainer} />
+// <Route exact path="/search" component={SearchContainer} />
+// <Route exact path="/profile" component={Profile} />
+// <Route exact path="/notebooks/create" component={CreateNotebookForm} />
+// <Route exact path="/outlines/outlinenote/create/:id" component={CreateOutlineNoteForm} />
+// <Route exact path="/outlines/:id" component={UserOutlineContainer} />
+// <Route exact path="/notebooks/:id" component={UserNotebookContainer} />
+
+class App extends Component {
+  render(){
+
+    return (
+      <Container>
+        <NavBar />
+        <Switch>
+
+          <Route exact path="/" render={props =>
+            this.props.user ?
+              <SearchContainer />
+              : <Redirect to='/OAuth'/>
+          }/>
+          <Route exact path="/OAuth" component={GoogleAuthContainer} />
+
+        </Switch>
+      </Container>
+    );
+  }
 };
 
-export default App;
+let mapStateToProps = ({ user }, ownProps) => {
+  return { user };
+};
+
+export default withRouter(connect(mapStateToProps)(App));
