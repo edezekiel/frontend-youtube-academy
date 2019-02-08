0d56ef09212bc7fa8457bfc11d9154c6bedff880
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-08 13:22:56 -0500

    delete the AppRouter component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
 
-import store from './redux/store'
-import AppRouter from './AppRouter'
+import { Switch, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Provider store={store}>
-            <AppRouter />
-        </Provider>
-      </BrowserRouter>
-    );
-  }
-}
+import NavBar from "./NavBar";
+
+import RootContainer from "./container/RootContainer";
+import GoogleAuthContainer from "./container/GoogleAuthContainer";
+import SearchContainer from "./container/SearchContainer";
+import Profile from "./container/Profile";
+import UserOutlineContainer from "./container/UserOutlineContainer";
+import UserNotebookContainer from "./container/UserNotebookContainer";
+
+import CreateNotebookForm from "./forms/CreateNotebookForm";
+import CreateOutlineNoteForm from "./forms/CreateOutlineNoteForm";
+
+const App = props => {
+  return (
+    <Container>
+      <NavBar />
+      <Switch>
+        <Route exact path="/logout" component={GoogleAuthContainer} />
+        <Route exact path="/search" component={SearchContainer} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/notebooks/create" component={CreateNotebookForm} />
+        <Route exact path="/outlines/outlinenote/create/:id" component={CreateOutlineNoteForm} />
+        <Route exact path="/outlines/:id" component={UserOutlineContainer} />
+        <Route exact path="/notebooks/:id" component={UserNotebookContainer} />
+        <Route exact path="/" component={RootContainer} />
+      </Switch>
+    </Container>
+  );
+};
 
 export default App;
