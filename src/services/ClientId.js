a4d80a22d89fa05f8afd39504a6dfc88806e86f7
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-02-11 10:33:14 -0500

    create new client id to debub netlify

--- src/services/ClientId.js
+++ src/services/ClientId.js
@@ -1,3 +1,3 @@
-const CLIENT_ID = '369548765069-mof01apa10i4vj53dk7g327j8oh91te7.apps.googleusercontent.com'
+const CLIENT_ID = '369548765069-ojg2qi20p8od8ej6dgls9eg6kcbag5pj.apps.googleusercontent.com'
 
 export default CLIENT_ID
